6bc23d18f4eba0a8a5dfff68c65f8a210d5a59d2
Whitefallen <t.kammann1614@web.de>
2023-02-05 15:48:05 +0100

    added basic randomness behavior

--- bot.js
+++ bot.js
@@ -1,7 +1,13 @@
 const tmi = require('tmi.js');
 const axios = require("axios").default;
+const _ = require('lodash');
+let channelsSongArray = [];
+
 // Prepare Channels List
-const channels = process.env.CHANNELS.split(',');
+let allChannels = process.env.CHANNELS;
+let channels = allChannels.includes(',') ? allChannels.split(',') : allChannels;
+
+const songlistData = require("./songlist.json");
 
 // Define configuration options
 const opts = {
@@ -19,7 +25,7 @@ const client = new tmi.client(opts);
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
-
+client.on('disconnected', onDisconnectedHandler);
 // Connect to Twitch:
 client.connect();
 
@@ -29,31 +35,60 @@ function onMessageHandler (target, context, msg, self) {
 
   // Remove whitespace from chat message
   const msgArr = msg.split(' ');
-  // Command name [!bsr] args
+  // Command name [!white-radio] args
   const commandName = msgArr[0];
-  // Command Args !bsr [args]
-  const commandArgs = msgArr[1];
+  // Command name !white-radio [args]
+  const commandValue = msgArr[1];
   // Get username
   const sender = context.username;
   // Get Channel the command was issued
   const channel = target.substring(1, target.length);
 
+  const allGenresAvailable = Object.keys(songlistData).filter(key => songlistData[key].length)
   // If the command is known, let's execute it
-  if (commandName === '!white-radio') {
+  if (commandName.startsWith("!") && commandName === '!white-radio') {
     // save SongRequest
-    sendRandomSong(sender, channel);
+    if(commandValue) {
+      if(allGenresAvailable.includes(commandValue)) {
+        sendRandomSongFromGenre(channel, commandValue);
+      } else {
+       client.say(channel, `I dont have music for that genre, sorry :(`);
+      }
+    } else {
+      sendRandomSong(channel);
+    }
 
     console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
   }
 }
 
-async function sendRandomSong(username, channel) {
-  client.say(channel, `!sr https://www.youtube.com/watch?v=WvmnqWBczHo`);
+async function sendRandomSong(channel) {
+  let song = pickRandomAcrossGenres();
+  client.say(channel, `!sr ${song}`);
+}
+
+async function sendRandomSongFromGenre(channel, genre) {
+  let song = pickRandomGenres(genre);
+  client.say(channel, `!sr ${song}`);
+}
+
+function pickRandomAcrossGenres() {
+  let songlist = [];
+  for(let genre in songlistData) {
+    songlist.push(songlistData[genre]);
+  }
+  return _.sample(_.flattenDeep(songlist).filter((el) => el !== null));
+}
+
+function pickRandomGenres(genre) {
+  return _.sample(songlistData[genre].filter((el) => el !== null));
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+}
+
+function onDisconnectedHandler() {
+
 }
